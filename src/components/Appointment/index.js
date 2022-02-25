632d168dff87a174cbae4a13628d93ddeee89bb1
BoNeZ4Lyfe <NUR.MAHAD@GMAIL.COM>
2022-02-25 05:48:42 -0500

    create appointment views

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,10 +2,6 @@ import React from "react";
 
 import "components/Appointment/styles.scss";
 
-export default function Appointment (props) {
-  
-    return (
-      <article className="appointment"></article>
-    );
-
-    };
+export default function Appointment(props) {
+  return <article className="appointment"></article>;
+}
