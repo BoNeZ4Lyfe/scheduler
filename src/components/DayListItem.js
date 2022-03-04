edfdab3dee1d64ba0a3d45c3c401c3b4996fa3fe
BoNeZ4Lyfe <NUR.MAHAD@GMAIL.COM>
2022-03-04 07:41:03 -0500

    add cypress/navigate days

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -25,6 +25,7 @@ export default function DayListItem(props) {
         props.setDay(props.name);
       }}
       className={dayListClass}
+      data-testid='day'
     >
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{formatSpots()}</h3>
